2e4660d370312d3f956121adc1a05f7fd57b10b5
meu <msaraujo1008@gmail.com>
2023-03-16 15:59:43 -0300

    Primeiro

--- aluguel/urls.py
+++ aluguel/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from .views import index, lista_car, realizar_aluguel, detalhar_car, realizar_aluguel_car,cadastrar_cliente,cadastrar_car
 
 urlpatterns = [
-    path(''),
+    path('', index, name='index'),
+    path('car/', lista_car, name='listar_car'),
+    path('car/<int:pk>', detalhar_car, name='detalhar_car'),
+    path('car/aluguel/<int:car_pk>', realizar_aluguel_car, name='realizar_aluguel_car' ),
+    path('car/add',cadastrar_car, name='cadastrar_carro'),
+    path('aluguel/add', realizar_aluguel, name='realizar_aluguel'),
+    path('clientes/add',cadastrar_cliente, name='cadastrar_cliente')
+    
 ]
