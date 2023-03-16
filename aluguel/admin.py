600e101329bc1a1f856fa4f7c726490c6467cf5d
meu <msaraujo1008@gmail.com>
2023-03-16 15:36:14 -0300

    Primeiro

--- aluguel/admin.py
+++ aluguel/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cliente, Car, Aluguel
+from .forms import ClienteForm, CarForm, AluguelForm
 
 # Register your models here.
--- aluguel/models.py
+++ aluguel/models.py
@@ -32,10 +32,9 @@ class Aluguel(models.Model):
     data_devolucao = models.DateField("Data de devolução")
     valor = models.DecimalField("Valor", max_digits=15, decimal_places=2)
     devolucao = models.BooleanField("Devolvido")
-    cliente = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING, related_name='cliente_alugueis', verbose_name="Cliente")
     diaria = models.DecimalField("Diaria",max_digits=10, decimal_places=2)
     carro = models.ForeignKey(Car, on_delete=models.DO_NOTHING, related_name='carro_alugueis', verbose_name="Carro")
-    cpf = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING, related_name='cliente_alugueis') 
+    cpf = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING, related_name='cliente_alugueis')
 
     def __str__(self):
         return "{} - {} - {}".format(self.codigo, self.cliente.nome, self.carro.modelo)
